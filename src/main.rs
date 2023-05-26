3d5a99552c2ff321ebfec84cc715116058014645
Martin Klapacz <martin.klapacz@tum.de>
2023-05-27 01:03:11 +0200

    separate thread for gRPC service

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use log::{info, LevelFilter};
 use tokio::io::AsyncWriteExt;
 use tokio::net::{TcpListener, TcpStream};
 use tokio::task::JoinHandle;
-use tonic::transport::Server;
+use tonic::transport::{Channel, Server};
 
 use crate::chord::chord_proto::chord_server::ChordServer;
 use crate::chord::ChordService;
@@ -31,27 +31,40 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let grpc_addr = args.address;
     let tcp_addr = "127.0.0.1:50052";
 
+    let mut thread_handles = Vec::new();
+
+    // if args.peer.is_some() {
+    //     info!("Joining existing cluster");
+    //     let peer_address = args.peer.unwrap().as_str();
+    //     let channel = Channel::from_static(peer_address)
+    //         .connect()
+    //         .await?;
+    // } else {
+    //     info!("Starting new cluster")
+    // }
+
 
     info!("Starting up tcp main thread");
-    let tcp_thread_handle: JoinHandle<()> = tokio::spawn(async move {
+    thread_handles.push(tokio::spawn(async move {
         let listener = TcpListener::bind(tcp_addr).await.unwrap();
         loop {
             let (socket, _) = listener.accept().await.unwrap();
             info!("New client connection established");
             tokio::spawn(async move { handle_client_connection(socket).await.unwrap() });
         }
-    });
+    }));
 
     info!("Starting up gRPC service");
-    let grpc_thread_handle = tokio::spawn(async move {
+    thread_handles.push(tokio::spawn(async move {
         let chord_service = ChordService::new(&grpc_addr, 32);
         Server::builder()
             .add_service(ChordServer::new(chord_service))
             .serve(grpc_addr.parse().unwrap())
-    });
+    }));
 
-    tcp_thread_handle.await?;
-    grpc_thread_handle.await?;
+    for handle in thread_handles {
+        let _ = handle.await?;
+    }
 
     Ok(())
 }
