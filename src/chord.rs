782069f8a884725eed91f5dd3cf12cec57469c44
Martin Klapacz <martin.klapacz@tum.de>
2023-06-09 16:24:31 +0200

    worked on update others

--- src/chord.rs
+++ src/chord.rs
@@ -20,7 +20,7 @@ pub type Address = String;
 #[derive(Debug)]
 pub struct ChordService {
     address: String,
-    key: Key,
+    pos: Key,
     finger_table: Arc<Mutex<FingerTable>>,
     predecessor: Arc<Mutex<FingerEntry>>,
 }
@@ -31,7 +31,7 @@ impl ChordService {
         let (finger_table, predecessor) = rx.await.unwrap();
         ChordService {
             address: url.clone(),
-            key: crypto::hash(&url),
+            pos: crypto::hash(&url),
             finger_table: Arc::new(Mutex::new(finger_table)),
             predecessor: Arc::new(Mutex::new(predecessor)),
         }
@@ -39,7 +39,7 @@ impl ChordService {
 
     pub fn is_successor_of_key(&self, key: Key) -> bool {
         let predecessor_position = self.predecessor.lock().unwrap().key.clone();
-        let own_position = self.key.clone();
+        let own_position = self.pos.clone();
 
         if predecessor_position < own_position {
             return predecessor_position < key && key <= own_position;
@@ -49,21 +49,34 @@ impl ChordService {
             return true;
         }
     }
+}
 
-    async fn find_successor_helper(&self, id: Key) -> Result<FingerEntry, Box<dyn Error>> {
-        let all_fingers = &self.finger_table.lock().unwrap().fingers;
-        let successor_fingers: Vec<FingerEntry> = all_fingers.iter()
-            .filter(|finger| id <= finger.key)
-            .map(|finger| finger.clone())
-            .collect();
-        let closest_successor_finger = match successor_fingers.is_empty() {
-            true => all_fingers.first(),
-            false => successor_fingers.first()
-        }.unwrap().clone();
-
-        Ok(closest_successor_finger)
+pub fn is_between(key: Key, lower: Key, upper: Key, left_open: bool, right_open: bool) -> bool {
+    if lower < upper {
+        if left_open && right_open {
+            return lower < key && key < upper;
+        } else if left_open {
+            return lower < key && key <= upper;
+        } else if right_open {
+            return lower <= key && key < upper;
+        } else {
+            return lower <= key && key <= upper;
+        }
+    } else if lower > upper {
+        if left_open && right_open {
+            return lower < key || key < upper;
+        } else if left_open {
+            return lower < key || key <= upper;
+        } else if right_open {
+            return lower <= key || key < upper;
+        } else {
+            return lower <= key || key <= upper;
+        }
+    } else {
+        return !left_open && key == lower;
     }
 }
+
 
 #[tonic::async_trait]
 impl chord_proto::chord_server::Chord for ChordService {
@@ -75,7 +88,18 @@ impl chord_proto::chord_server::Chord for ChordService {
 
         let successor_finger_entry: FingerEntry = match self.is_successor_of_key(key_msg.into()) {
             true => self.address.clone().into(),
-            false => self.find_successor_helper(key_msg.into()).await.unwrap()
+            false => {
+                let find_predecessor_response = self.find_predecessor(Request::new(request.get_ref().clone()))
+                    .await
+                    .unwrap();
+                let mut predecessor_client = ChordClient::connect(format!("http://{}", find_predecessor_response.get_ref().address))
+                    .await
+                    .unwrap();
+                let successor_of_key_predecessor_response = predecessor_client.get_direct_successor(Request::new(Empty {}))
+                    .await
+                    .unwrap();
+                successor_of_key_predecessor_response.get_ref().address.clone().into()
+            }
         };
         info!("Received find_successor call for {:?}, successor is {:?}", key_msg, successor_finger_entry);
         Ok(Response::new(successor_finger_entry.into()))
@@ -83,16 +107,37 @@ impl chord_proto::chord_server::Chord for ChordService {
 
 
     async fn find_predecessor(&self, request: Request<KeyMsg>) -> Result<Response<AddressMsg>, Status> {
-        let successor_finger_entry = self.find_successor_helper(request.get_ref().into()).await.unwrap();
-        let mut client = ChordClient::connect(format!("http://{}", successor_finger_entry.address))
-            .await
-            .unwrap();
+        let look_up_key = Key::from_be_bytes(request.get_ref().key.clone().try_into().unwrap());
+        let mut current_address_finger_entry: FingerEntry = self.address.clone().into();
+        let mut current_successor_finger_entry: FingerEntry = {
+            let finger_table_guard = self.finger_table.lock().unwrap();
+            finger_table_guard.fingers[0].clone()
+        };
 
-        let predecessor = client.get_predecessor(Request::new(
-            Empty {}
-        )).await.unwrap().get_ref().clone();
+        let mut current_key: Key = current_address_finger_entry.clone().into();
+        let mut current_successor_key: Key = crypto::hash(&current_successor_finger_entry.clone().address);
 
-        Ok(Response::new(predecessor))
+        while !is_between(look_up_key, current_key, current_successor_key, true, false) {
+            let mut current_client = ChordClient::connect(format!("http://{}", current_successor_finger_entry.address))
+                .await
+                .unwrap();
+            let response = current_client.find_closest_preceding_finger(Request::new(KeyMsg {
+                key: look_up_key.to_be_bytes().to_vec(),
+            })).await.unwrap();
+            current_address_finger_entry = response.get_ref().clone().into();
+            current_key = current_address_finger_entry.key;
+
+            current_client = ChordClient::connect(format!("http://{}", current_address_finger_entry.address))
+                .await
+                .unwrap();
+            let response = current_client.get_direct_successor(Request::new(Empty {}))
+                .await
+                .unwrap();
+            current_successor_finger_entry = response.get_ref().into();
+            current_successor_key = current_successor_finger_entry.key;
+        }
+
+        Ok(Response::new(current_address_finger_entry.into()))
     }
 
     async fn get_predecessor(&self, _request: Request<Empty>) -> Result<Response<AddressMsg>, Status> {
@@ -110,6 +155,11 @@ impl chord_proto::chord_server::Chord for ChordService {
         Ok(Response::new(Data {}))
     }
 
+    async fn get_direct_successor(&self, _: Request<Empty>) -> Result<Response<AddressMsg>, Status> {
+        let finger_table_guard = self.finger_table.lock().unwrap().fingers[0].clone();
+        Ok(Response::new(finger_table_guard.into()))
+    }
+
     async fn update_finger_table_entry(&self, request: Request<UpdateFingerTableEntryRequest>) -> Result<Response<Empty>, Status> {
         let index_to_update = request.get_ref().index as usize;
         let finger_entry_update: &FingerEntry = &(request.get_ref().clone().finger_entry.unwrap().into());
@@ -119,26 +169,49 @@ impl chord_proto::chord_server::Chord for ChordService {
             let predecessor_guard = self.predecessor.lock().unwrap();
             predecessor_guard.address.clone()
         };
-        let mut upper_finger = {
+        let upper_finger = {
             let finger_table_guard = self.finger_table.lock().unwrap();
-            finger_table_guard.fingers[index_to_update].key
+            finger_table_guard.fingers[index_to_update].clone()
         };
-        if self.key <= finger_entry_update_key && finger_entry_update_key < upper_finger {
+
+
+        let upper_key = crypto::hash(&upper_finger.address);
+        if is_between(finger_entry_update_key, self.pos, upper_key, false, true)
+            || self.pos == upper_key {
             info!("Updating finger entry {} with {:?}", index_to_update, finger_entry_update);
             {
                 let mut finger_table_guard = self.finger_table.lock().unwrap();
-                finger_table_guard.fingers[index_to_update] = finger_entry_update.clone();
+                finger_table_guard.fingers[index_to_update].address = finger_entry_update.address.clone();
             }
-            let mut predecessor_to_update_client = ChordClient::connect(format!("http://{}", predecessor_address_str))
-                .await
-                .unwrap();
-            let _ = predecessor_to_update_client.update_finger_table_entry(Request::new(UpdateFingerTableEntryRequest {
-                index: index_to_update as u32,
-                finger_entry: Some(finger_entry_update.into())
-            })).await.unwrap();
+
+            if predecessor_address_str.ne(self.address.as_str()) {
+                let mut predecessor_to_update_client = ChordClient::connect(format!("http://{}", predecessor_address_str))
+                    .await
+                    .unwrap();
+                let _ = predecessor_to_update_client.update_finger_table_entry(Request::new(UpdateFingerTableEntryRequest {
+                    index: index_to_update as u32,
+                    finger_entry: Some(finger_entry_update.into()),
+                })).await.unwrap();
+            }
         }
 
-        Ok(Response::new(Empty{}))
+        Ok(Response::new(Empty {}))
+    }
+
+    async fn find_closest_preceding_finger(&self, request: Request<KeyMsg>) -> Result<Response<FingerEntryMsg>, Status> {
+        let key = Key::from_be_bytes(request.get_ref().clone().key.try_into().unwrap());
+        for finger in self.finger_table.lock().unwrap().fingers.iter().rev() {
+            if is_between(finger.key, self.pos, key, false, false) {
+                return Ok(Response::new(FingerEntryMsg {
+                    id: finger.key.to_be_bytes().to_vec(),
+                    address: finger.clone().address.into(),
+                }));
+            }
+        }
+        Ok(Response::new(FingerEntryMsg {
+            id: self.pos.to_be_bytes().to_vec(),
+            address: self.address.clone().into(),
+        }))
     }
 
 
@@ -148,7 +221,7 @@ impl chord_proto::chord_server::Chord for ChordService {
 
         Ok(Response::new(NodeSummaryMsg {
             url: self.address.clone(),
-            id: self.key.to_be_bytes().iter().map(|byte| byte.to_string()).collect::<Vec<String>>().join(" "),
+            id: self.pos.to_be_bytes().iter().map(|byte| byte.to_string()).collect::<Vec<String>>().join(" "),
             predecessor: Some(predecessor.into()),
             finger_entries: finger_table_guard.fingers.iter()
                 .map(|finger| finger.clone())
--- src/join.rs
+++ src/join.rs
@@ -1,16 +1,19 @@
+use std::convert::TryInto;
 use std::error::Error;
 use std::ops::Add;
+
 use log::info;
 use tokio::sync::oneshot::Sender;
 use tonic::Request;
+
 use crate::chord::Address;
+use crate::chord::chord_proto::{AddressMsg, Empty, UpdateFingerTableEntryRequest};
 use crate::chord::chord_proto::chord_client::ChordClient;
-use crate::chord::chord_proto::{Empty, AddressMsg, UpdateFingerTableEntryRequest};
 use crate::crypto;
 use crate::crypto::{HashRingKey, Key};
 use crate::finger_table::{FingerEntry, FingerTable};
 
-pub async fn process_node_join(peer_address_option: Option<Address>, own_grpc_address_str: &String, tx: Sender<(FingerTable, FingerEntry)>) -> Result<(), Box<dyn Error>>{
+pub async fn process_node_join(peer_address_option: Option<Address>, own_grpc_address_str: &String, tx: Sender<(FingerTable, FingerEntry)>) -> Result<(), Box<dyn Error>> {
     let own_id = crypto::hash(own_grpc_address_str);
 
     let mut finger_table = FingerTable::new(&own_id, own_grpc_address_str);
@@ -35,7 +38,7 @@ pub async fn process_node_join(peer_address_option: Option<Address>, own_grpc_ad
             let mut direct_successor_client = ChordClient::connect(format!("http://{}", direct_successor_url))
                 .await
                 .unwrap();
-            let get_predecessor_response = direct_successor_client.get_predecessor(Request::new(Empty{})).await.unwrap();
+            let get_predecessor_response = direct_successor_client.get_predecessor(Request::new(Empty {})).await.unwrap();
             predecessor = get_predecessor_response.get_ref().clone().into();
             info!("Received predecessor from peer");
 
@@ -48,9 +51,12 @@ pub async fn process_node_join(peer_address_option: Option<Address>, own_grpc_ad
             let finger_entry_this: FingerEntry = own_grpc_address_str.into();
             info!("Updated predecessor of {:?} to {:?}", &finger_entry_peer, &finger_entry_this);
 
-            let mut index = 0;
-            for finger in &finger_table.fingers {
-                let key_to_find_predecessor_for: Key = own_id.overflowing_sub(Key::two().overflowing_pow(index ).0).0;
+            // finger table is constructed, send it to grpc thread so it can start up its service
+            tx.send((finger_table.clone(), predecessor.into())).unwrap();
+
+            info!("Updating others...");
+            for (index, finger) in finger_table.fingers.iter().enumerate() {
+                let key_to_find_predecessor_for: Key = own_id.overflowing_sub(Key::two().overflowing_pow(index as u32).0).0;
                 let response = join_peer_client.find_predecessor(Request::new(key_to_find_predecessor_for.into()))
                     .await
                     .unwrap();
@@ -59,9 +65,8 @@ pub async fn process_node_join(peer_address_option: Option<Address>, own_grpc_ad
                 let mut predecessor_to_update_client = ChordClient::connect(format!("http://{}", predecessor_to_update_address))
                     .await
                     .unwrap();
-                index += 1;
-                let _ = predecessor_to_update_client.update_finger_table_entry(Request::new(UpdateFingerTableEntryRequest{
-                    index,
+                let _ = predecessor_to_update_client.update_finger_table_entry(Request::new(UpdateFingerTableEntryRequest {
+                    index: index as u32,
                     finger_entry: Some(finger_entry_this.clone().into()),
                 })).await.unwrap();
             }
@@ -70,9 +75,9 @@ pub async fn process_node_join(peer_address_option: Option<Address>, own_grpc_ad
         None => {
             info!("Starting up a new cluster");
             finger_table.set_all_fingers(&own_grpc_address_str);
+            tx.send((finger_table, predecessor.into())).unwrap();
         }
     };
 
-    tx.send((finger_table, predecessor.into())).unwrap();
     Ok(())
 }
