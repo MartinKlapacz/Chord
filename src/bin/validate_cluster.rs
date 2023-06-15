8e266ad27fd25bfe06ebb033cd17b7d024515c3b
Martin Klapacz <martin.klapacz@tum.de>
2023-06-16 00:56:59 +0200

    validate fingers

--- src/bin/validate_cluster.rs
+++ src/bin/validate_cluster.rs
@@ -2,6 +2,7 @@ use tonic::Request;
 use tonic::transport::Channel;
 
 use chord::crypto;
+use chord::crypto::Key;
 
 use crate::chord_proto::{Empty, NodeSummaryMsg};
 use crate::chord_proto::chord_client::ChordClient;
@@ -30,9 +31,36 @@ async fn main() {
         a.id.parse::<u128>().unwrap().cmp(&b.id.parse::<u128>().unwrap())
     });
 
+    let node_ids: Vec<Key> = node_summaries.iter()
+        .map(|node_summary: &NodeSummaryMsg| node_summary.id.parse::<Key>().unwrap())
+        .collect::<Vec<Key>>();
+
+    // check predecessors
     for i in 0..node_summaries.len() {
-        if node_summaries[i].id.eq(&node_summaries[(i + 1) % node_summaries.len()].predecessor.clone().unwrap().id) {
-            print!("error")
+        let current_node: String = node_summaries[i].id.clone();
+        let next_node_pred: String = node_summaries[(i + 1) % node_summaries.len()].predecessor.clone().unwrap().id;
+
+        if current_node.ne(&next_node_pred) {
+            panic!("Node {} has wrong predecessor: {}", current_node, next_node_pred)
         }
     }
+
+    for i in 0..node_summaries.len() {
+        let fingers = &node_summaries[i].finger_entries;
+        for finger in fingers {
+            let finger_key: Key = finger.id.parse::<Key>().unwrap();
+            let node_pointed_to = crypto::hash(&finger.address);
+            let actually_responsible_node = get_responsible_node_for_key(finger_key, &node_ids);
+            assert_eq!(node_pointed_to, actually_responsible_node)
+        }
+    }
+
+    println!("looks good!")
+}
+
+fn get_responsible_node_for_key(key: Key, other_nodes: &Vec<Key>) -> Key {
+    *other_nodes.iter()
+        .filter(|&node| key < *node)
+        .min()
+        .unwrap_or(other_nodes.iter().min().unwrap())
 }
