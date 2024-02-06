a100f9728c6934f21bea6353abec62e600de2962
Martin Klapacz <klapacz.martin@gmail.com>
2024-02-06 13:23:53 +0100

    use python3 in put scripts

--- test/put_data2.sh
+++ test/put_data2.sh
@@ -4,5 +4,5 @@ PORT=5501
 
 for i in {51..100}
 do
-   python dht_client.py -a 127.0.0.1 -p $PORT -s -k $RANDOM -d hello$RANDOM
+   python3 dht_client.py -a 127.0.0.1 -p $PORT -s -k $RANDOM -d hello$RANDOM
 done
--- test/put_data3.sh
+++ test/put_data3.sh
@@ -4,5 +4,5 @@ PORT=5501
 
 for i in {101..150}
 do
-   python dht_client.py -a 127.0.0.1 -p $PORT -s -k $i -d hello$RANDOM
+   python3 dht_client.py -a 127.0.0.1 -p $PORT -s -k $i -d hello$RANDOM
 done
