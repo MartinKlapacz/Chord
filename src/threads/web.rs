03a9cd1c3fe7c39117a71f71ac212fe855ee13a7
Martin Klapacz <klapacz.martin@gmail.com>
2024-02-11 01:04:19 +0100

    added basic page template

--- src/threads/web.rs
+++ src/threads/web.rs
@@ -1,8 +1,19 @@
 use actix_web::{App, get, HttpResponse, post, Responder, web};
+use tera::{Tera, Context};
 
 #[get("/")]
 pub async fn index() -> impl Responder {
-    "Hello, World!"
+    let tera = Tera::new("templates/**/*").unwrap();
+
+    let mut context = Context::new();
+    context.insert("title", "My Web Page");
+    context.insert("greeting", "Hello, Actix-web with Tera!");
+
+    let rendered_html = tera.render("index.html", &context).unwrap();
+
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(rendered_html)
 }
 
 #[get("/{name}")]
