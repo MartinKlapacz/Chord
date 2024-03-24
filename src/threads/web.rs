d4e1ff13c91051dee937e4f6339c4cbe81fe4517
Martin Klapacz <klapacz.martin@gmail.com>
2024-03-24 20:49:17 +0100

    display response status upon get request

--- src/threads/web.rs
+++ src/threads/web.rs
@@ -3,11 +3,15 @@ use std::sync::{Arc, Mutex};
 use actix_web::{get, HttpResponse, Responder, web};
 use serde::Deserialize;
 use tera::{Context, Tera};
+use tonic::Request;
+use tonic::transport::Channel;
 
 use chord::utils::config::Config;
 use chord::utils::types::HashPos;
 
 use crate::node::finger_table::FingerTable;
+use crate::threads::chord::chord_proto::{GetRequest, GetStatus};
+use crate::threads::chord::chord_proto::chord_client::ChordClient;
 
 #[derive(Deserialize)]
 struct QueryParams {
@@ -19,14 +23,42 @@ struct QueryParams {
 pub async fn index(
     finger_table_data: web::Data<Arc<Mutex<FingerTable>>>,
     config: web::Data<Config>,
+    grpc_client: web::Data<Arc<Mutex<ChordClient<Channel>>>>,
     query_params_option: Option<web::Query<QueryParams>>,
 ) -> impl Responder {
     let tera = Tera::new("static/html/**/*").unwrap();
     let mut context = Context::new();
 
-    if let Some(query_params) = query_params_option {
-        // todo: get response from client
-        context.insert("get_response", &query_params.0.get_input);
+
+    if query_params_option.is_some() {
+        let get_key_string = query_params_option.unwrap().0.get_input;
+        let mut key_array: [u8; 32] = [0; 32];
+        for (i, c) in get_key_string.chars().enumerate() {
+            key_array[i] = c as u8;
+        }
+
+        let response = grpc_client.lock().unwrap().get(Request::new(GetRequest {
+            key: key_array.to_vec(),
+        })).await.unwrap();
+
+        match GetStatus::from_i32(response.get_ref().status) {
+            Some(GetStatus::Ok) => {
+                context.insert("response_status", "OK");
+                // let response_string = response.get_ref().value.as_bytes()
+                //     .to_vec()
+                //     .into_iter()
+                //     .map(|c| c.to_string())
+                //     .collect::<String>();
+                // context.insert("get_response", &response_string);
+            }
+            Some(GetStatus::NotFound) => {
+                context.insert("response_status", "NOT_FOUND");
+            }
+            Some(GetStatus::Expired) => {
+                context.insert("response_status", "EXPIRED");
+            }
+            _ => panic!("Received invalid get response status")
+        }
     }
 
     context.insert("title", "Chord Node");
